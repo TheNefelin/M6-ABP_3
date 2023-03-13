461073124004c702330c1117bac92b81a8f88c3f
TheNefelin <metalflap@gmail.com>
2023-03-13 17:50:17 -0300

    Update

--- data/data.js
+++ data/data.js
@@ -1,21 +1,5 @@
-const express = require("express");
-const app = express();
-app.set("view engine", "hbs");
-app.set("views", "./views");
-app.use(express.static("public"));
 
-const hbs = require("hbs");
-hbs.registerPartials(__dirname + "/views/partials");
-
-app.listen(3000);
-
-app.get("/", (req, res) => {
-    const data = JSON.parse(dt);
-    const arrData = Object.keys(data).map(key => data[key]);
-    res.render("index", {prueba: "Hola", arr: arrData});
-});
-
-const dt = `{
+export const dt = `{
     "localidad 1": { "Continente": "África", "País": "Angola", "Capital": "Luanda" }, 
     "localidad 2": { "Continente": "América del Norte", "País": "Estados Unidos", "Capital": "Washington DC" },
     "localidad 3": { "Continente": "América Central", "País": "México", "Capital": "Ciudad de México" }, 
@@ -25,4 +9,3 @@ const dt = `{
     "localidad 7": { "Continente": "Oceanía", "País": "Australia", "Capital": "Camberra" }, 
     "localidad 8": { "Continente": "Asia", "País": "Japón", "Capital": "Tokio" } 
 }`
-
