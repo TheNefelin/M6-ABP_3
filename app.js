6f9ef4974edd1702c1913b8b1077a8d53aa34839
TheNefelin <metalflap@gmail.com>
2023-03-13 17:23:10 -0300

    Update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const { json } = require("body-parser");
 const express = require("express");
 const app = express();
 app.set("view engine", "hbs");
@@ -12,30 +11,5 @@ app.listen(3000);
 
 app.get("/", (req, res) => {
     res.render("index");
-    // res.send(paises);
-    const obj = JSON.parse(data());
-    console.log(obj)
 });
 
-function data() {
-    const dt = `{
-        "localidad 1": 
-            { "Continente": "África", "País": "Angola", "Capital": "Luanda" }, 
-        "localidad 2": 
-            { "Continente": "América del Norte", "País": "Estados Unidos", "Capital": "Washington DC" },
-        "localidad 3": 
-            { "Continente": "América Central", "País": "México", "Capital": "Ciudad de México" }, 
-        "localidad 4": 
-            { "Continente": "América del Sur", "País": "Brasil", "Capital": "Brasilia" },
-        "localidad 5": 
-            { "Continente": "Europa", "País": "España", "Capital": "Madrid" }, 
-        "localidad 6": 
-            { "Continente": "Europa", "País": "Alemania", "Capital": "Berlín" }, 
-        "localidad 7": 
-            { "Continente": "Oceanía", "País": "Australia", "Capital": "Camberra" }, 
-        "localidad 8": 
-            { "Continente": "Asia", "País": "Japón", "Capital": "Tokio" } 
-    } `
-
-    return dt
-}
--- public/index.js
+++ public/index.js
@@ -1,7 +1,36 @@
 
+const dt = `{
+    "localidad 1": { "Continente": "África", "País": "Angola", "Capital": "Luanda" }, 
+    "localidad 2": { "Continente": "América del Norte", "País": "Estados Unidos", "Capital": "Washington DC" },
+    "localidad 3": { "Continente": "América Central", "País": "México", "Capital": "Ciudad de México" }, 
+    "localidad 4": { "Continente": "América del Sur", "País": "Brasil", "Capital": "Brasilia" }, 
+    "localidad 5": { "Continente": "Europa", "País": "España", "Capital": "Madrid" }, 
+    "localidad 6": { "Continente": "Europa", "País": "Alemania", "Capital": "Berlín" }, 
+    "localidad 7": { "Continente": "Oceanía", "País": "Australia", "Capital": "Camberra" }, 
+    "localidad 8": { "Continente": "Asia", "País": "Japón", "Capital": "Tokio" } 
+}`
+
+const data = JSON.parse(dt);
+var arrData = Object.keys(data).map(key => data[key]);
+
 const body = document.querySelector("body");
 
-const div = document.createElement("div");
-div.innerText = "JS Publico";
+arrData.forEach(e => {
+    const tarjetaContenedor = document.createElement("div");
+    tarjetaContenedor.classList.add("tarjeta-contenedor");
 
-body.appendChild(div);
+    const continente = document.createElement("div");
+    continente.innerText = e.Continente;
+    tarjetaContenedor.appendChild(continente);
+
+    const pais = document.createElement("div");
+    pais.innerText = e.País;
+    tarjetaContenedor.appendChild(pais);
+    
+    const capital = document.createElement("div");
+    capital.innerText = e.Capital;
+    tarjetaContenedor.appendChild(capital);
+
+    body.appendChild(tarjetaContenedor);
+});
+
